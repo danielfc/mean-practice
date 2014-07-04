72bf5edce1a29ceedbd4d7b2e4a606d674f3eb87
Daniel Campos <daniel.felicissimo@gmail.com>
2014-07-04 09:57:42 -0300

    Adding Stylus and some Express 4 configs

--- server.js
+++ server.js
@@ -1,10 +1,35 @@
-var express = require('express');
+var express = require('express'),
+	stylus = require('stylus'),
+	logger = require('morgan'),
+	bodyParser = require('body-parser');
 
+//Manage the Node environment variable 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
+
+//Create the compile function to pass to Stylus pré-processor
+function compile(str, path) {
+	return stylus(str).set('filename', path);
+}
+
+//Some Express configurations
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
+
+//In replace to express.logger('dev')
+app.use(logger('dev'));
+//In replace to express.bodyParser()
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+app.use(stylus.middleware({
+	src: __dirname + '/public',
+	compile: compile
+}));
+app.use(express.static(__dirname + '/public'));
+
 
 app.get('*', function (req, res) {
 	res.render('index');
